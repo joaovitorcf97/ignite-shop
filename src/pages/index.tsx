043f79395ba6cdbdc2d24798c2c0ba552aaf6c1f
João Vitor <joaovitorcfdev@gmail.com>
2023-03-07 22:38:17 -0300

    criando styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 8,
+  border: 'none',
+  padding: '4px 8px',
+
+  '&:hover': {
+    filter: 'brightness(0.8)',
+    cursor: 'pointer'
+  }
+})
+
 export default function Home() {
   return (
-    <h1>Hello</h1>
+    <Button>Enviar</Button>
   )
 }
